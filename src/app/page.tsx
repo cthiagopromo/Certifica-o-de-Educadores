376836995f90da4461c5334d3179e6f36134290d
Thiago promo <cthiagopromo@gmail.com>
2025-09-04 18:14:07 +0000

    mudar apenas essa imagem da seção Quem Somos e deixar sempre responsivel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -235,7 +235,7 @@ export default function Home() {
                         <div className="flex-1 self-stretch flex flex-col justify-start items-start gap-4">
                             <div className="self-stretch flex flex-col sm:flex-row justify-start items-start gap-4">
                                 <Image className="flex-1 h-80 object-cover rounded-2xl" width={416} height={320} src="https://i.postimg.cc/xjwv6vFH/professora-amarela-s-educador.webp" alt="Professora com blusa amarela" data-ai-hint="teacher smiling" />
-                                <Image className="flex-1 h-80 object-cover rounded-2xl" width={417} height={320} src="https://picsum.photos/417/320" alt="Teacher helping student" data-ai-hint="teacher student" />
+                                <Image className="flex-1 h-80 object-cover rounded-2xl" width={417} height={320} src="https://i.postimg.cc/RZDVTRgx/professora-azul-s-educador.webp" alt="Teacher helping student" data-ai-hint="teacher student" />
                             </div>
                             <div className="self-stretch p-8 md:py-14 md:px-12 bg-primary rounded-2xl flex flex-col justify-center items-center text-center">
                                 <div className="max-w-[53rem] flex flex-col justify-start items-start">
