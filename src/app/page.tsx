085384ba8da7ce7fd43441f2ebd3c74e772411d3
Thiago promo <cthiagopromo@gmail.com>
2025-09-04 19:34:17 +0000

    // Com  text-gray-100 text-xl font-medium font-['Rethink_Sans'] leading-

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
                         <div className="self-stretch flex flex-col justify-start items-start gap-6">
                             <h1 className="self-stretch text-white text-4xl font-bold leading-10">Imagine ser lembrado como o professor(a) que mudou a vida de seus alunos. Abrimos inscrições para a Certificação de Educadores.</h1>
                             <p className="self-stretch text-gray-100 text-xl font-medium leading-normal">
-                              <span className="text-[#F8B6FE]">Com bolsas de 50% de desconto.</span> Acreditamos que cada professor carrega dentro de si o poder de transformar o futuro.
+                              <span className="text-[#F8B6FE] font-bold">Com bolsas de 50% de desconto.</span> Acreditamos que cada professor carrega dentro de si o poder de transformar o futuro.
                             </p>
                         </div>
                         <div className="w-full flex flex-col sm:flex-row justify-start items-center gap-5">
