2740d300c6f1b2bf126f4f0bf41a1349c1403092
Thiago promo <cthiagopromo@gmail.com>
2025-09-04 16:55:34 +0000

    aumentar a logo do rodapé em 100% (_for element img_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -253,7 +253,7 @@ export default function Home() {
             <div className="flex flex-col items-center justify-center gap-8">
                 <div className="flex flex-col items-center gap-2">
                     <Link href="/" className="flex items-center gap-2">
-                        <Image src="https://soeducador.com.br/assets/img/site/logo/logo-wide-white-full.png" alt="SóEducador Logo" width={90} height={14} className="h-auto" />
+                        <Image src="https://soeducador.com.br/assets/img/site/logo/logo-wide-white-full.png" alt="SóEducador Logo" width={180} height={28} className="h-auto" />
                     </Link>
                     <p className="text-base text-primary-foreground/90 flex items-center gap-2">Inspirando você a mudar o mundo 🌎</p>
                 </div>
